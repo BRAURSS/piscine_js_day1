7f1684107d166997df03b9f21723ea4121dacc0c
bilal <bilal.malem@epitech.digital>
2025-04-27 12:54:17 +0200

    ADD: all day 1

--- ex_03.js
+++ ex_03.js
@@ -1,4 +1,32 @@
-let number1 = 1;
-let number2 = 2;
-let combined = number1.toString() + number2.toString();
-displayThisText(combined);
+function addsandwiches(){
+    let hamsandwiches = 0
+    let tunasandwiches = 0
+    let vegetariansandwiches = 0
+
+    while(breadCount >= 2 && hamCount >= 1){
+        hamsandwiches++
+        breadCount -= 2
+        hamCount -= 1
+    }
+
+    while(breadCount >= 2 && tunaCount >= 1 && tomatoCount >= 2){
+        tunasandwiches++
+        breadCount -= 2
+        tunaCount -= 1
+        tomatoCount -= 2
+    }
+
+    while(breadCount >= 2 && saladCount >= 1 && tomatoCount >= 2){
+        vegetariansandwiches++
+        breadCount -= 2
+        saladCount -= 1
+        tomatoCount -= 2
+    }
+
+    displayThisText("Number of ham sandwich(es) : " + hamsandwiches)
+    displayThisText("Number of tuna sandwich(es) : " + tunasandwiches)
+    displayThisText("Number of vegetarian sandwich(es) : " + vegetariansandwiches)
+
+    displayIngredients()
+}
+addsandwiches()
